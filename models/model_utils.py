deb1f286dd805429cb6e8187723d42395aee559c
YZ <zhyzh91@gmail.com>
2024-01-28 23:41:50 -0500

    Create model_utils.py

---
+++ models/model_utils.py
@@ -0,0 +1,76 @@
+from collections import OrderedDict
+from os.path import join
+import math
+import pdb
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Attn_Net_Gated(nn.Module):
+    def __init__(self, L = 1024, D = 256, dropout = False, n_classes = 1):
+        r"""
+        Attention Network with Sigmoid Gating (3 fc layers)
+
+        args:
+            L (int): input feature dimension
+            D (int): hidden layer dimension
+            dropout (bool): whether to apply dropout (p = 0.25)
+            n_classes (int): number of classes
+        """
+        super(Attn_Net_Gated, self).__init__()
+        self.attention_a = [
+            nn.Linear(L, D),
+            nn.Tanh()]
+        
+        self.attention_b = [nn.Linear(L, D), nn.Sigmoid()]
+        if dropout:
+            self.attention_a.append(nn.Dropout(0.25))
+            self.attention_b.append(nn.Dropout(0.25))
+
+        self.attention_a = nn.Sequential(*self.attention_a)
+        self.attention_b = nn.Sequential(*self.attention_b)
+        self.attention_c = nn.Linear(D, n_classes)
+
+    def forward(self, x):
+        a = self.attention_a(x)
+        b = self.attention_b(x)
+        A = a.mul(b)
+        A = self.attention_c(A)  # N x n_classes
+        return A, x
+
+
+def init_max_weights(module):
+    r"""
+    Initialize Weights function.
+
+    args:
+        modules (torch.nn.Module): Initalize weight using normal distribution
+    """
+    import math
+    import torch.nn as nn
+    
+    for m in module.modules():
+        if type(m) == nn.Linear:
+            stdv = 1. / math.sqrt(m.weight.size(1))
+            m.weight.data.normal_(0, stdv)
+            m.bias.data.zero_()
+    
+def SNN_Block(dim1, dim2, dropout=0.25):
+    r"""
+    Multilayer Reception Block w/ Self-Normalization (Linear + ELU + Alpha Dropout)
+
+    args:
+        dim1 (int): Dimension of input features
+        dim2 (int): Dimension of output features
+        dropout (float): Dropout rate
+    """
+    import torch.nn as nn
+
+    return nn.Sequential(
+            nn.Linear(dim1, dim2),
+            nn.ELU(),
+            nn.AlphaDropout(p=dropout, inplace=False))
